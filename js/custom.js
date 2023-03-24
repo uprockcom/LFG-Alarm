bdebb5411e69c975e8a6314df64176e49147c183
Jesse Adams <adamsj888@gmail.com>
2023-03-24 05:50:25 -0700

    add bitsignal alarm section

--- js/custom.js
+++ js/custom.js
@@ -60,6 +60,15 @@
               }
             ]
          });
+
+         $('.bitsignal-slider').slick({
+          dots: true,
+          infinite: true,
+          speed: 300,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          autoplay: false,
+         });
          
          const targetDate = new Date("June 15, 2023");
 
@@ -71,12 +80,40 @@
          const daysLeft = Math.floor(timeDiff / (1000 * 3600 * 24));
          
          // Update the HTML element with the result
-         $('.bitsignal-deadline').html(daysLeft + " days left");
+         $('.bitsignal-deadline').html(daysLeft);
          
 
          $('.video-wrapper').click(function() {
           $(this).find('.play-button, .video-placeholder').fadeOut(500);
           $(this).find('.video-embed').fadeIn(500).attr('src', $(this).find('.video-embed').attr('src') + '?autoplay=1').css('z-index', '0');
+        });
+
+        $('.subscribe-btn').click(function() {
+          $(this).toggleClass('active');
+          if ($(this).hasClass('active')) {
+            $(this).html('SUBSCRIBED');
+          } else {
+            $(this).html('$' + $(this).text());
+          }
+        });
+
+        $("#copy-btn").click(function() {
+          // Copy URL to clipboard
+          var url = "https://lfgalarm.com";
+          var tempInput = $("<input>");
+          $("body").append(tempInput);
+          tempInput.val(url).select();
+          document.execCommand("copy");
+          tempInput.remove();
+          
+          // Show pillbox and fade out after 1 second
+          var pillbox = $("<div>");
+          pillbox.addClass("pillbox");
+          pillbox.text("Copied!");
+          $(this).append(pillbox);
+          pillbox.fadeIn(100).delay(1000).fadeOut(100, function() {
+            $(this).remove();
+          });
         });
         
     });
